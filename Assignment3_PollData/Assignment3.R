77e34bc4fbc11ac566bfb64fa5392d5f8c242bb3
heidy200 <heidy20@usf.edu>
2025-09-14 02:19:25 -0400

    Update Assignment3.R

--- Assignment3_PollData/Assignment3.R
+++ Assignment3_PollData/Assignment3.R
@@ -1,2 +1,25 @@
+# Assignment_01.R
 
-lol
+# Define vectors
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_poll <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_poll <- c(12, 75, 43, 19, 1, 21, 19)
+
+# Combine into a data frame
+df_polls <- data.frame(Name, ABC_poll, CBS_poll)
+
+# Inspect structure and preview
+str(df_polls)
+head(df_polls)
+
+# Summary statistics
+mean(df_polls$ABC_poll)
+median(df_polls$CBS_poll)
+range(df_polls[, c("ABC_poll", "CBS_poll")])
+
+# Add difference column
+df_polls$Diff <- df_polls$CBS_poll - df_polls$ABC_poll
+
+# View updated data frame
+print(df_polls)
+
