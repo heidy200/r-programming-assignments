e384d34aa3c12609382afd7ef0ac893289521129
heidy200 <heidy20@usf.edu>
2025-09-29 01:05:57 -0400

    Update Assignment5.R

--- Assignment5_Matrix/Assignment5.R
+++ Assignment5_Matrix/Assignment5.R
@@ -11,6 +11,10 @@ dim(B)  # 10 × 100 — not square
 invA <- solve(A)
 detA <- det(A)
 
+# Inspect Results
+invA   
+detA 
+
 # For B, use tryCatch to capture errors
 invB <- tryCatch(solve(B), error = function(e) e)
 detB <- tryCatch(det(B),   error = function(e) e)
